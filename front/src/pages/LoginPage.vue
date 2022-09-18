83821673fc5255a59f23d55822198ee72b101f34
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-09-18 20:31:46 +0400

    add login template

--- front/src/pages/LoginPage.vue
+++ front/src/pages/LoginPage.vue
@@ -1,3 +1,21 @@
 <template>
-  <v-btn>login page</v-btn>
+  <form @submit.prevent="" class="root">
+    <v-text-field label="Логин"></v-text-field>
+    <v-text-field label="Пароль"></v-text-field>
+    <v-btn type="submit">Войти</v-btn>
+  </form>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginPage",
+});
+</script>
+
+<style scoped>
+.root {
+  width: 650px;
+}
+</style>
