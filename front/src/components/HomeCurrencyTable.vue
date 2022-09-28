d024d5ee024f49aa846e775b2fd3d21f50d2d103
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-09-28 15:55:14 +0400

    add table

---
+++ front/src/components/HomeCurrencyTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card width="65%" height="400px" prepend-icon="mdi-home" variant="outlined">
+    <template v-slot:title>Курс валют</template>
+    <v-card-text>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">Валюта</th>
+            <th class="text-left">Курс</th>
+            <th class="text-left">Дата</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="{ key, rate, date } in data" :key="key">
+            <td>{{ key }}</td>
+            <td>{{ rate }}</td>
+            <td>{{ date }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeCurrencyTableComponent",
+  props: ["data"],
+});
+</script>
--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -6,43 +6,19 @@
   <Loader v-if="isLoading" />
   <div v-else class="flex">
     <HomeCurrencyBill :data="curriencies" />
-    <v-card
-      width="65%"
-      height="400px"
-      prepend-icon="mdi-home"
-      variant="outlined"
-    >
-      <template v-slot:title>Курс валют</template>
-      <v-card-text>
-        <v-table>
-          <thead>
-            <tr>
-              <th class="text-left">Валюта</th>
-              <th class="text-left">Курс</th>
-              <th class="text-left">Дата</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="{} in curriencies" :key="item.name">
-              <td>{{ item.name }}</td>
-              <td>{{ item.calories }}</td>
-              <td>{{ item.calories }}</td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-card-text>
-    </v-card>
+    <HomeCurrencyTable :data="curriencies" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HomeCurrencyBill from "@/components/HomeCurrencyBill.vue";
+import HomeCurrencyTable from "@/components/HomeCurrencyTable.vue";
 
 export default defineComponent({
   name: "HomePage",
   data: () => ({ isLoading: true, curriencies: null }),
-  components: { HomeCurrencyBill },
+  components: { HomeCurrencyBill, HomeCurrencyTable },
   async mounted() {
     try {
       const curriencies = await this.$store.dispatch("getCurrencies");
