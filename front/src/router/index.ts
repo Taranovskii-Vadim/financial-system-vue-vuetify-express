bca8f8b9c4c454303308993ecdbad077ebd11357
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-10-06 19:36:39 +0400

    protect routes

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -16,43 +16,43 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/HomePage.vue"),
   },
   {
     path: "/categories",
     name: "categories",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/CategoriesPage.vue"),
   },
   {
     path: "/record",
     name: "record",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/RecordPage.vue"),
   },
   {
     path: "/profile",
     name: "profile",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/ProfilePage.vue"),
   },
   {
     path: "/planning",
     name: "planning",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/PlanningPage.vue"),
   },
   {
     path: "/detail",
     name: "detail",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/DetailPage.vue"),
   },
   {
     path: "/history",
     name: "history",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../pages/HistoryPage.vue"),
   },
 ];
@@ -60,6 +60,17 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const token = document.cookie.split("token=")[1];
+  const isAuth = to.matched.some((item) => item.meta.auth);
+
+  if (isAuth && !token) {
+    next("/signIn");
+  } else {
+    next();
+  }
 });
 
 export default router;
